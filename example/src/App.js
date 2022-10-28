5c35d644ed75892cfe639017b96623269c68c458
harundogdu <harundogdu06@gmail.com>
2022-10-28 12:07:52 +0300

    feat: tiff viewer props changed

--- example/src/App.js
+++ example/src/App.js
@@ -2,10 +2,20 @@ import React from 'react'
 
 import { TIFFViewer } from 'react-tiff-viewer'
 import 'react-tiff-viewer/dist/index.css'
-import tiffFile from './multipage.tiff'
+import tiffFile from './images/multipage.tiff'
+import tiffFile2 from './images/sample.tiff'
+import tiffFile3 from './images/test.tiff'
+import tiffFile4 from './images/balloons.tif'
 
 const App = () => {
-  return <TIFFViewer tiff={tiffFile} />
+  return (
+    <TIFFViewer
+      tiff={tiffFile}
+      lang='tr'
+      paginate='ltr'
+      buttonColor='#141414'
+    />
+  )
 }
 
 export default App
